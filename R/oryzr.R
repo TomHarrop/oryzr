b0f04816f4a3c43dd44a4522a68ac90770d7c66a
Tom Harrop <twharrop@gmail.com>
2015-04-24 17:35:20 +0200

    added cultivar name to docs

--- R/oryzr.R
+++ R/oryzr.R
@@ -1,17 +1,16 @@
 #' oryzr: Work with Oryza sativa gene identifiers.
 #' 
 #' Convert between different types of gene identifiers for \emph{Oryza sativa} 
-#' Nipponbare genes (MSU, Rap-DB, CSGNL, RefSeq mRNA).
+#' L. ssp. \emph{japonica} cv. Nipponbare genes (MSU, Rap-DB, CSGNL, RefSeq
+#' mRNA).
 #' 
-#' @section Functions: \itemize{
-#'   \item \code{\link{LocToGeneName}}: Convert MSU 
+#' @section Functions: \itemize{ \item \code{\link{LocToGeneName}}: Convert MSU 
 #'   identifiers to CSGNL gene names and symbols and generate a label for 
-#'   plotting if requested.
-#'   \item \code{\link{LocToRefSeq}}: Convert MSU
+#'   plotting if requested. \item \code{\link{LocToRefSeq}}: Convert MSU 
 #'   identifiers to Rap-DB identifiers and map them to RefSeq mRNA identifiers. 
 #'   }
 #'   
 #' @docType package
 #' @name oryzr
 
-NULL
+NULL 
