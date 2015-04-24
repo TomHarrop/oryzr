3e4e4a767c2c8dde5ed08ddfc6ddeaffa836066c
Tom Harrop <twharrop@gmail.com>
2015-04-24 12:51:15 +0200

    Added code for generating data. Started documenation.

---
+++ R/oryzr.R
@@ -0,0 +1,31 @@
+#' oryzr: Work with Oryza sativa gene identifiers.
+#'
+#' Convert between different types of gene identifiers for \emph{Oryza sativa} Nipponbare genes (MSU, Rap-DB, CSGNL, RefSeq mRNA).
+#'  
+#' @section Functions:
+#' The oryzr functions ...
+#'
+#' @docType package
+#' @name oryzr
+
+NULL
+
+#' Test function.
+#' 
+#' This function doesn't do anything.
+#' 
+#' This awesome function doesn't do anything, it just returns the input. Blahdy 
+#' blah blah blah.
+#' 
+#' @param x Anything
+#' @return Returns the input. Bof sadfasdf  asdfkljsd dfjfkladjf sadfjsdkf
+#'   asdfjsdf sjdfsdfjskldf fjjja asdlfjk asdfl
+#' @export
+#' @examples
+#' foo('abc')
+#' foo(42)
+#' foo(TRUE)
+
+foo <- function(x) {
+  return(x)
+}
