ade95fa208dbb007d3560cbb1e265f2e0a27df65
Tom Harrop <twharrop@gmail.com>
2015-04-29 15:14:58 +0200

    Formatting

--- R/utils.R
+++ R/utils.R
@@ -2,5 +2,24 @@
 #' 
 #' date formatted for console messages
 dateF <- function() {
-    return(format(Sys.time(), "[%a %b %d %H:%M:%S %Y]"))
+  return(format(Sys.time(), "[%a %b %d %H:%M:%S %Y]"))
 } 
+
+#' showDateRetrieved
+#' 
+#' Show the dates when the databases (RAP-DB to MSU IDs, OryzabaseGeneListEn and
+#' Rice ID IRGSP Build5 database) were last downloaded to generate
+#' \code{R/sysdata.rda}.
+#' @return Returns a named character vector of date retrieved for each database.
+showDateRetrieved <- function() {
+  getDatestamps <- function(sysData){
+    env <- new.env()
+    load(sysData, env)
+    c(
+      RapMsuRefSeq = attr(env[['RapMsuRefSeq']], 'dateRetrieved'),
+      RAPMSU = attr(env[['RAPMSU']], 'dateRetrieved'),
+      GeneListByID.frame = attr(env[['GeneListByID.frame']], 'dateRetrieved')
+    )
+  }
+  getDatestamps('R/sysdata.rda')
+}
