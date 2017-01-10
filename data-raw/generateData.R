ea5ce459210d72369dea283846beb488310c249c
Tom Harrop <twharrop@gmail.com>
2017-01-10 13:30:26 +1300

    Changed to Rice ID IRGSP ver1.0 as it appears to have more RefSeq IDs

--- data-raw/generateData.R
+++ data-raw/generateData.R
@@ -135,7 +135,7 @@ attr(msu.annotation.collapsed, "dateRetrieved") <- Sys.time()
 # download data.table of rice id build table from oryzabase
 temp <- tempfile()
 download.file(
-  "http://www.shigen.nig.ac.jp/rice/oryzabase/tool/riceIdChecker/build5/download", 
+  "http://shigen.nig.ac.jp/rice/oryzabase/tool/riceIdChecker/download", 
   method = "auto", temp)
 RapMsuRefSeq <- data.table::data.table(read.table(unzip(temp, unzip(temp, 
     list = TRUE)[, 1], exdir = "data-raw", overwrite = TRUE), header = TRUE, 
